0033937a392748ca2ec94ccfedb24120aaa26500
abhishek <abhisheksharma425@gmail.com>
2026-09-11 16:40:50 +0530

    five character secret

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=vault_url, credential=credential)
     secret = client.get_secret("DbConnectionString")
-    return f"DB secret starts with: {secret.value}..."
+    return f"DB secret starts with: {secret.value[:5]}..."
 
 if __name__ == "__main__":
     app.run()
