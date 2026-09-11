75275b9a43633872ff05a5e4a2a77a1965b08b79
abhishek <abhisheksharma425@gmail.com>
2026-09-11 17:18:52 +0530

    full secrets

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=vault_url, credential=credential)
     secret = client.get_secret("DbConnectionString")
-    return f"DB secret starts with: {secret.value[:1]}..."
+    return f"DB secret starts with: {secret.value}..."
 
 if __name__ == "__main__":
     app.run()
